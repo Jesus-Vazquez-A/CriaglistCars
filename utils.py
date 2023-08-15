6765275157dc7b5cd92f700f13ff557c992a5486
Amado de Jesús Vázquez Acuñas <85312561+Jesus-Vazquez-A@users.noreply.github.com>
2023-08-14 19:18:48 -0600

    Add files via upload

--- utils.py
+++ utils.py
@@ -12,13 +12,13 @@ import json
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
-
+import warnings
 
 
 # In[2]:
 
 
-
+warnings.filterwarnings('ignore')
 
 
 # In[3]:
@@ -46,14 +46,14 @@ def loadJsonFile():
            return json_file
 
 
-# In[6]:
+# In[7]:
 
 
 class Model(nn.Module):
     def __init__(self):
         super(Model,self).__init__()
 
-        self.fc1 = nn.Linear(in_features = 56,out_features = 64)
+        self.fc1 = nn.Linear(in_features = 425,out_features = 64)
         self.fc2 = nn.Linear(in_features = 64,out_features = 32)
         self.fc3 = nn.Linear(in_features= 32,out_features=16)
         self.output = nn.Linear(in_features = 16,out_features = 1)
@@ -66,9 +66,10 @@ class Model(nn.Module):
 
         return self.output(x)
     
+        
 def loadModel():
     model = Model()
-    model.to('cpu')
-    model.load_state_dict(torch.load("craiglist_model.pth",map_location ='cpu'))
+    model.load_state_dict(torch.load("model.pth",map_location=torch.device('cpu')))
     return model
     
+
